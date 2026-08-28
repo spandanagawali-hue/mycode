f9edd4da67de0bcbcefc20ab8eff4c2d818c253f
spandanagawali-hue <spandana.gawali@gmail.com>
2026-08-29 05:13:14 +0530

    removed link

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         />
         <meta
           name="keywords"
-          content="YouTube, coffee, links, video, sharing, free, online, tool"
+          content="YouTube, coffee,video, sharing, free, online, tool"
         />
       </Helmet>
       <LandingPage />
